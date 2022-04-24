cdb5f44cb2a1475e5189ece663268a5778e17709
Jean-Paul Dosher <87234390+jpdosher@users.noreply.github.com>
2022-04-24 16:14:54 -0300

    formatação estética

--- pomo.py
+++ pomo.py
@@ -7,19 +7,29 @@ root  = Tk()
 root.title("PomoDoro")  #inserir emoji/ícone e status do programa(talvez)
 #root.iconbitmap('')
 root.geometry('600x400')
+root.configure(bg='#fc5549')
 
 def clock():
     hour = time.strftime("%H")
     minute = time.strftime("%M")
     second = time.strftime("%S")
+    my_label.config(text = hour + ":" + minute + ":" +second)
+    my_label.after(1000, clock)
 
 def update():
-    my_label.config(text = "new text")
+    my_label.config(text="New Text")
 
-my_label = Label(root, text="Old Text")
+
+my_label = Label(root, text="", font=("Helvetica",  48), fg = 'white', bg= '#ff675c' )
 my_label.pack(pady=20)
 
-my_label.after(5000, update)
+my_label2 = Label(root, text="")
+
+
+
+clock()
+
+#my_label.after(5000, update)
 
 
 
